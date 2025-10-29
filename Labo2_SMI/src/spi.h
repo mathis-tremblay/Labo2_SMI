e51c51d4afba244a27a5d13a63cacce6319f06bb
GuiLaroche <gular105@ulaval.ca>
2025-10-28 23:05:09 -0400

    task list

---
+++ Labo2_SMI/src/spi.h
@@ -0,0 +1,10 @@
+// TODO
+
+// Module SPI pour initialiser pis faire fonctionner le LCD
+
+// Completer les fonctions de lcd_driver_src
+
+// Activer la bonne clk
+
+// Editer registres, et mettre mode alternatif sur GPIO
+
