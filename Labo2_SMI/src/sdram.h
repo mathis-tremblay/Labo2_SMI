4498bbe6a9a75990c44bc967f2ece88e59c0e6f4
GuiLaroche2 <guibenmat38@gmail.com>
2025-11-11 15:41:47 -0500

    edited sdram.h to add functions

--- Labo2_SMI/src/sdram.h
+++ Labo2_SMI/src/sdram.h
@@ -4,4 +4,10 @@
 
 void SDRAM_Init(void);
 
+void SDRAM_Write(uint32_t address, uint16_t data);
+
+uint16_t SDRAM_Read(uint32_t address);
+
+void SDRAM_WriteArray(uint32_t address, const uint16_t* data, uint32_t length);
+
 #endif /* SDRAM_H_ */
